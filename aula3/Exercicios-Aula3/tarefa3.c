13c452374ab71406b2575f6832380482b3466cbc
daltonbr <sacinopatinete@gmail.com>
2015-10-16 09:39:18 -0300

    [wip] tarefa3-aula3 - Index Map

--- aula3/Exercicios-Aula3/tarefa3.c
+++ aula3/Exercicios-Aula3/tarefa3.c
@@ -2,18 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
+void createIndexMap(FILE* arq, int arr[]);
+
 int main() {
 
   int arr[10];
-
   FILE *arq;
 
-  if((arq = fopen("campo_var_reg_var.dad", "r+") == NULL)) {
-    puts("Fodeu");
+  if((arq = fopen("campo_var_reg_var.dad", "r+")) == NULL) {
+    puts("Arquivo nao encontrado!");
   } else {
     createIndexMap(arq, arr);
   }
 
+// Debug: print the IndexMap
+int i;
+for (i = 0; i < 10; i++ )
+{
+    printf("\n #%d - Value: %d", i, arr[i]);
+}
 
   return 0;
 }
@@ -24,12 +31,21 @@ void createIndexMap(FILE* arq, int arr[]) {
   int size = 0;
   int position = 0;
 
+  printf("\nCreating IndexMap...");
   while (size != EOF) {
-    fread(size, sizeof(char), 1, arq);
-    arr[position] = (int) size;
-    printf("%d\n", arr[position]);
-    position++;
-    fseek(arq, (int) size, SET_CUR);
+    if( fread(&size, sizeof(char), 1, arq) ) //Return NULL when reach EOF
+    {
+        arr[position] = (int) size;
+        printf("\nDEBUG: #%d - Value: %d", position, arr[position]);
+        position++;
+        fseek(arq, (int)size, 1);  //SET_CUR nao aceito como parametro. Why?
+    }
+    else
+    {
+        printf("\nDebug: EOF reached");
+        size = -1;
+    }
   }
+
 
 }
